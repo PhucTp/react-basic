cd79f2edef0a1a0d550dcf5d3fa10b86cce254e1
pqlong <phamquanglong.hcm@vnpt.vn>
2023-12-20 16:53:48 +0700

    ok P

--- src/components/componentSieuThi/SieuThiList.jsx
+++ src/components/componentSieuThi/SieuThiList.jsx
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useState } from "react";
 import DataTable from "react-data-table-component";
 import { getSieuThiList } from "../../api/api.js";
 
 const SieuThiList = () => {
+  const title = "Submit to show store list";
+  const keyword = useRef("");
+  const size = useRef(5);
+  const page = useRef(1);
+  const tinhst = useRef("");
+  const huyenst = useRef("");
+  const xast = useRef("");
+  const fromDate = useRef("");
+  const toDate = useRef("");
+  const [sieuthi, setSieuThi] = useState();
+
+  function checkVaildInput(input) {
+    if (!input) {
+      console.log("Please enter");
+    }
+    console.log(input);
+  }
+
+  const input = {
+    page: page.current,
+    pageSize: size.current,
+    keyword: keyword.current,
+    rageDate: {
+      fromDate: fromDate.current,
+      toDate: toDate.current,
+    },
+    tinhst: tinhst.current,
+    huyenst: huyenst.current,
+    xast: xast.current,
+  };
+
   const columns = [
     {
       name: "MaST",
@@ -27,22 +58,10 @@ const SieuThiList = () => {
     },
   ];
 
-  const [sieuthi, setSieuThi] = useState();
-
   const GetSieuThiList = async () => {
     try {
-      const res = await getSieuThiList({
-        page: 1,
-        pageSize: 10,
-        // keyword: "string",
-        // rageDate: {
-        //   fromDate: "2023-12-18T15:30:58.326Z",
-        //   toDate: "2023-12-18T15:30:58.326Z",
-        // },
-        // tinhst: "string",
-        // huyenst: "string",
-        // xast: "string",
-      });
+      checkVaildInput(input);
+      const res = await getSieuThiList(input);
       setSieuThi(res);
     } catch (e) {
       console.log(e);
@@ -51,8 +70,50 @@ const SieuThiList = () => {
 
   return (
     <div>
-      <button onClick={GetSieuThiList}> Submit to show store list</button>
-      <p>Tổng số Siêu Thị {sieuthi?.totalCount}</p>
+      <div>
+        <div>
+          <input
+            type="text"
+            placeholder="Tên"
+            ref={keyword}
+            value={keyword.current.value}
+          />
+          <input
+            type="number"
+            placeholder="pageSize"
+            ref={size}
+            value={size.current.value}
+          />
+          <input
+            type="number"
+            placeholder="page"
+            ref={page}
+            value={page.current.value}
+          />
+          <input
+            type="text"
+            placeholder="Tỉnh"
+            ref={tinhst}
+            value={tinhst.current.value}
+          />
+          <input
+            type="text"
+            placeholder="Huyện"
+            ref={huyenst}
+            value={huyenst.current.value}
+          />
+          <input
+            type="text"
+            placeholder="Xã"
+            ref={xast}
+            value={xast.current.value}
+          />
+        </div>
+        <div>
+          <button onClick={GetSieuThiList}>{title}</button>
+          <p>Tổng số Siêu Thị {sieuthi?.totalCount}</p>
+        </div>
+      </div>
       <DataTable
         title="Sieu Thi Data"
         columns={columns}
